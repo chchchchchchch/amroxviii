4db3810f87e91c7fcd2f393e607f2c00597de00d
christoph <christoph@lafkon.net>
2018-03-16 12:20:14 +0100

    provide figlet font as argument (optional)

--- E/figlet2stroke.sh
+++ E/figlet2stroke.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
- TEXT=$*
- FONT=`ls ../lib/fonts/figlet/*.flf | shuf -n 1`
  CFONT="OCRA-Medium"
- TMPDIR=.
- TMP=$TMPDIR/${RANDOM}
- OUTDIR=.
+ OUTDIR=".";TMPDIR=".";TMP="$TMPDIR/${RANDOM}"
+ FONTDIR="../lib/fonts/figlet"
+ FONT=`echo $* | sed 's/ /\n/g' | grep "flf$"`
+ if [ "$FONT" == "" ]
+ then TEXT="$*"
+      FONT=`ls $FONTDIR/*.flf | shuf -n 1`
+ else TEXT=`echo "$*" | sed "s,[ ]*$FONT[ ]*,,g"`
+ fi 
 
  if [ `echo $TEXT | wc -c` -lt 2 ]; then 
  echo; echo "We need some text!"
@@ -16,7 +19,7 @@
  figlet -w 80 -f $FONT ;echo
  read -p "SAVE THIS? [y/n] " ANSWER
  if [ X$ANSWER != Xy ]; then 
-      echo "Try another one!";echo
+      echo "Try again!";echo
       exit 0;
  else
       OUTPUT=${OUTDIR}/`date +%Y%m%d%H%M%S`.pdf
